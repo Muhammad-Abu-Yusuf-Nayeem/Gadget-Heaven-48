e7228996fd005b291be1218757591e67d9e84caf
assignment-01 <ykhan9463@gmail.com>
2025-10-27 22:45:38 +0600

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex justify-evenly gap-6">
+      <div className="hero bg-base-200 h-auto my-12 py-12">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="hero bg-base-200 h-auto my-12 py-12">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
